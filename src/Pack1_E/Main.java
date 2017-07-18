e27ca39b4224a261a0c2e991096cf4b43ac3ad4b
henryfoster <andreev.1.eduard@gmail.com>
2017-07-18 10:44:05 +0200

    v0.1

--- src/Pack1_E/Main.java
+++ src/Pack1_E/Main.java
@@ -2,10 +2,31 @@ package Pack1_E;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// Test
-		
+    public static void main(String[] args) {
+        Graph_start g = new Graph_start (15);
+        g.knotenneu (3);
+        g.knotenneu (5);
+        g.kanteneu  (3,5,21);
+        g.knotenneu (4);
+        g.kanteneu  (3,4,17);
+        g.kanteneu  (4,5,13);
+        g.knotenneu (8);
+        g.kanteneu  (8,3,29);
 
-	}
+        g.ausgabe();
+
+       // g.knotenneu (5);
+       // g.kanteneu  (8,5,20);
+        //g.kanteneu  (8,3,29);
+       // g.ausgabe();
+        //		Fehler, weil Knoten fehlt
+       // g.kanteneu  (7,8,29);
+       // g.knotenneu (7);
+       // g.kanteneu  (7,8,29);
+        //g.ausgabe();
+ 
+    }
+
+
 
 }
