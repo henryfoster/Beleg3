a6504a24415943001c78714071ef19eee0d39669
NexusNoteNutzer <NexusNoteNutzer@DESKTOP-IISE20G.fritz.box>
2017-07-24 23:38:30 +0200

    test2

--- src/Pack1_E/Graph_start.java
+++ src/Pack1_E/Graph_start.java
@@ -275,5 +275,5 @@ public class Graph_start {
 	        
 	    }
 
-	// fakultativ Breitensuche, Tiefensuche oder Wegsuchehgtcdcgt
+	// fakultativ Breitensuche, Tiefensuche oder Wegsuchehgtcdcgth
 }
